b835d1926dba9f703e5a1b1b6ed3b9177c83de21
sejinpark0209 <sejinpark0209@gmail.com>
2017-04-14 14:45:51 -0700

    Step 5 Complete. Added and connected controllers

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var db = require('./models');
+var controllers = require('./controllers');
 
 app.use(express.static('public'));
-
 
 app.get('/', function homepage(req, res) {
 	res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api', controllers.api.index);
+
+app.get('/api/albums', functionName);
+
+function functionName(req, res) {};
 
 
 
